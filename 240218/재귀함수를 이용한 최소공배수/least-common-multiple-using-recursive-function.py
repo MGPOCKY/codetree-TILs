2956da039056096fbd692a0d43d594dd0a186e0f
Kyoungmin Kim <kkm1447@naver.com>
2024-02-18 18:13:32 +0900

    Title: 재귀함수를 이용한 최소공배수, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240218/재귀함수를 이용한 최소공배수/least-common-multiple-using-recursive-function.py
@@ -0,0 +1,13 @@
+def recursion(nums, val):
+    isAllSatisfy = True
+    for num in nums:
+        if val % num != 0:
+            isAllSatisfy = False
+    if isAllSatisfy:
+        return val
+    else:
+        return recursion(nums, val + nums[0])
+
+n = int(input())
+nums = list(map(int, input().split()))
+print(recursion(nums, num[0]))
